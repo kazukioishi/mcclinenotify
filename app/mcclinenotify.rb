22a41d9762ace0d266f12d530efdd3e7c14590eb
Kazuki Oishi <oishikazuki@gmail.com>
2017-04-06 03:44:28 +0900

    fixed coding style.

--- app/mcclinenotify.rb
+++ app/mcclinenotify.rb
@@ -10,18 +10,14 @@ cookie = agent.cookie_jar.cookies('https://circleapp.jp/top')
 p CookieTool::gethash(cookie)
 # get json
 conn = Faraday.new do |faraday|
-  faraday.request  :url_encoded             # form-encode POST params
-  faraday.response :logger                  # log requests to STDOUT
-  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  faraday.request :url_encoded # form-encode POST params
+  faraday.response :logger # log requests to STDOUT
+  faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
 end
 res = conn.post('https://circleapp.jp/mailinglist/listJSON/2738/p/0',
-                   JSON.generate({
-                       mode: '0',
-                       keyword: ''
-                   }), {
-                       'Content-Type': 'application/json; charset=UTF-8',
-                       'User-Agent': agent.user_agent,
-                       'Cookie': CookieTool::gethash(cookie)
-                   }
-)
-mljson = JSON.load(res.body)
+                JSON.generate({ mode: '0', keyword: '' }),
+                { 'Content-Type': 'application/json; charset=UTF-8',
+                  'User-Agent': agent.user_agent,
+                  'Cookie': CookieTool::gethash(cookie)
+                })
+mljson = JSON.parse(res.body)
